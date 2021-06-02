ef0d1d7962f3c615d16d9886e3ba188dc892751b
Michael Nußbaumer <m.nussbaumer@go-west.at>
2021-06-02 13:30:23 +0200

    Remove all content elements as we moved them to DCE

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,53 +1,6 @@
 <?php
 if(!defined('TYPO3_MODE')) die ('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'MediaImage',
-    []
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'MediaVideo',
-    []
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'LandingpageMediaImage',
-    []
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'UspTeaser',
-    ['Usp' => 'index']
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'ImageText',
-    []
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'HtmlVideo',
-    []
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'IframeVideo',
-    []
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'Quote',
-    []
-);
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'GoWest.gowest_contentelements',
@@ -55,44 +8,6 @@ if(!defined('TYPO3_MODE')) die ('Access denied.');
     []
 );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'LegalDisclosure',
-    ['Legaldisclosure' => 'index']
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'CsvTables',
-    ['Csvtables' => 'index']
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'Kennzahlen',
-    []
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'AnchorNavigation',
-    []
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'Faq',
-    []
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'GoWest.gowest_contentelements',
-    'ImageGallery',
-    []
-);
-
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('gowest_contentelements','setup',    ' <INCLUDE_TYPOSCRIPT: source="FILE:EXT:gowest_contentelements/Configuration/TypoScript/setup.typoscript">');
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['searchresult'] = \GoWest\GowestContentelements\Controller\SearchresultController::class . '::indexAction';
 
 
@@ -128,62 +43,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Core\DataHandling\DataHa
 $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 
 $iconRegistry->registerIcon(
-   'gowestcontentelements-mediaimage',
-   \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
-    'gowestcontentelements-mediavideo',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
-    'gowestcontentelements-landingpagemediaimage',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
-    'gowestcontentelements-uspteaser',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
-    'gowestcontentelements-imagetext',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
-    'gowestcontentelements-htmlvideo',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
-    'gowestcontentelements-iframevideo',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
-    'gowestcontentelements-quote',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
     'gowestcontentelements-smartsearch',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
-    'gowestcontentelements-legaldisclosure',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
-    'gowestcontentelements-csvtables',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
-);
-$iconRegistry->registerIcon(
-    'gowestcontentelements-kennzahlen',
     \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
     ['source' => 'EXT:gowest_contentelements/ext_icon.svg']
 );
