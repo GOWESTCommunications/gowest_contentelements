da17d1444c5e2ad9e3662e926250d38965c6fb45
MichaelNussbaumerGOWEST <m.nussbaumer@go-west.at>
2021-08-19 15:37:04 +0700

    Add categories to files
    Add code to have categories for the files. Only default language is checked.

--- Classes/Xclass/Headless/FileUtility.php
+++ Classes/Xclass/Headless/FileUtility.php
@@ -15,6 +15,9 @@ namespace GoWest\GowestContentelements\Xclass\Headless;
 
 use TYPO3\CMS\Core\Resource\AbstractFile;
 use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Connection;
 
 /**
  * Class FileUtility
@@ -32,6 +35,38 @@ class FileUtility extends \FriendsOfTYPO3\Headless\Utility\FileUtility
     {
         $return = parent::processFile($fileReference, $dimensions, $cropVariant);
         $metaData = $fileReference->toArray();
+
+        // Add categories of default language to files
+        $metaDataUid = $fileReference->getProperty('metadata_uid');
+        if($metaDataUid > 0) {
+            $dbConnection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('sys_category_record_mm');
+
+            $query = "
+                SELECT 
+                    c.*
+                FROM
+                    sys_category_record_mm cmm
+                    JOIN sys_file_metadata meta ON meta.uid = cmm.uid_foreign
+                    JOIN sys_category c ON c.uid = cmm.uid_local
+                WHERE
+                    cmm.tablenames = 'sys_file_metadata' 
+                    AND cmm.uid_foreign = " . $metaDataUid . "
+                ;
+            ";
+
+            $statement = $dbConnection->prepare($query);
+            $statement->execute();
+            $fileCategories = [];
+            while ($row = $statement->fetch()) {
+                $fileCategories[] = [
+                    "uid"       => $row['uid'],
+                    "parent"    => $row['parent'],
+                    "title"     => $row['title'],
+                ];
+            }
+
+            $return['properties']['categories'] = $fileCategories;
+        }
 
         // temporary fix for pdf thumbnail
         if($fileReference->getType() !== AbstractFile::FILETYPE_IMAGE && $fileReference->getMimeType() === 'application/pdf') {
